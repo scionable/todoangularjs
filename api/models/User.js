094bcf9da0f4b2ef0c88a891e04b8f4fece857fd
Arthor <arthor_serdyuk@ukr.net>
2018-04-24 01:17:53 +0300

    Added starting implementation of register user api

--- api/models/User.js
+++ api/models/User.js
@@ -1,8 +1,15 @@
 const mongoose = require('mongoose');
+var findOrCreate = require('mongoose-findorcreate');
 
-module.exports = mongoose.model('User', {
+const UserSchema = mongoose.Schema({
 	name : String,
 	email: String,
 	password: String,
 	isLoggedIn: Boolean
 });
+
+UserSchema.plugin(findOrCreate);
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User;
--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,79 +1,73 @@
 let Task = require('../models/Task');
 let User = require('../models/User');
 
-module.exports = function(app) {
-  app.get('/getAllTasks', (req, res) => {
-    Task.find((err, todos) => {
-      if (err) res.send(err);
+module.exports = function (app) {
 
-      res.json(todos);
-    });
-  });
+	app.get('/getAllTasks', (req, res) => {
+		Task.find((err, todos) => {
+			if (err) res.send(err);
 
-  app.post('/createTask', (req, res) => {
-    Task.create(
-      {
-        text: req.body.text,
-        day: req.body.day,
-        done: false
-      },
-      function(err, todo) {
-        if (err) res.send(err);
-        res.json(todo);
-      }
-    );
-  });
+			res.json(todos);
+		});
+	});
 
-  app.delete('/tasks/:todo_id', (req, res) => {
-    Task.remove(
-      {
-        _id: req.params.todo_id
-      },
-      function(err, todo) {
-        if (err) res.send(err);
+	app.post('/createTask', (req, res) => {
+		Task.create(
+			{
+				text: req.body.text,
+				day: req.body.day,
+				done: false
+			},
+			function (err, todo) {
+				if (err) res.send(err);
+				res.json(todo);
+			}
+		);
+	});
 
-        Task.find((err, todos) => {
-          if (err) res.send(err);
+	app.delete('/tasks/:todo_id', (req, res) => {
+		Task.remove(
+			{
+				_id: req.params.todo_id
+			},
+			function (err, todo) {
+				if (err) res.send(err);
 
-          res.json(todos);
-        });
-      }
-    );
-  });
+				Task.find((err, todos) => {
+					if (err) res.send(err);
 
-  app.get('*', (req, res) => {
-    res.sendfile('./assets/index.html');
-  });
+					res.json(todos);
+				});
+			}
+		);
+	});
+
+	app.get('*', (req, res) => {
+		res.sendfile('./assets/index.html');
+	});
 
 	app.patch('/completeTask', (req, res) => {
-		Task.findByIdAndUpdate({ _id: req.body.id }, { $set: { done: req.body.done }}, { new: true }, function (err, task) {
+		Task.findByIdAndUpdate({_id: req.body.id}, {$set: {done: req.body.done}}, {new: true}, function (err, task) {
 			if (err) res.send(err);
 			res.send(task);
 		});
 	});
 
-
 	app.post('/registerUser', (req, res) => {
-		User.find((err, todos) => {
-			if (err) res.send(err);
+		let name = req.body.name || undefined;
+		let email = req.body.email || undefined;
+		let password = req.body.password || undefined;
 
-			res.json(todos);
-		});
-
-		User.create({
-			name: req.body.name,
-			email: req.body.email,
-			password: req.body.password
-		}, function (err, user) {
-			if (err) res.send(err);
-			res.json(user);
+		User.findOrCreate({name: name, email: email, password: password}, function(err, user, created) {
+			if(err) res.send(err);
+			if(!created) {
+				res.send(`${user} already exist`);
+			} else {
+				res.json(user);
+				console.log('A new user been created');
+			}
 		});
 	});
-
-
-
-
-
 
 	app.get('*', (req, res) => {
 		res.sendfile('./assets/index.html');
