299d64bdbe2b156d40d333dc8b9798de04006bc9
Nadiia Bilozerova <nadiia.bilozerova@synapse.com>
2018-04-23 15:35:31 +0300

    fixed filter for count all tasks of each day

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -1,43 +1,53 @@
 let Task = require('../models/Task');
 
-module.exports = function (app) {
+module.exports = function(app) {
+  app.get('/getAllTasks', (req, res) => {
+    Task.find((err, todos) => {
+      if (err) res.send(err);
 
-	app.get('/getAllTasks', (req, res) => {
-		Task.find((err, todos) => {
-			if (err) res.send(err);
+      res.json(todos);
+    });
+  });
 
-			res.json(todos);
-		});
-	});
+  app.post('/createTask', (req, res) => {
+    Task.create(
+      {
+        text: req.body.text,
+        day: req.body.day,
+        done: false
+      },
+      function(err, todo) {
+        if (err) res.send(err);
+        res.json(todo);
+      }
+    );
+  });
 
-	app.post('/createTask', (req, res) => {
-		Task.create({
-			text: req.body.text,
-			day: req.body.day,
-			done: false
-		}, function (err, todo) {
-			if (err) res.send(err);
-			res.json(todo);
-		});
-	});
+  app.delete('/tasks/:todo_id', (req, res) => {
+    Task.remove(
+      {
+        _id: req.params.todo_id
+      },
+      function(err, todo) {
+        if (err) res.send(err);
 
-	app.delete('/tasks/:todo_id', (req, res) => {
-		Task.remove({
-			_id: req.params.todo_id
-		}, function (err, todo) {
-			if (err) res.send(err);
+        Task.find((err, todos) => {
+          if (err) res.send(err);
 
-			Task.find((err, todos) => {
-				if (err) res.send(err);
+          res.json(todos);
+        });
+      }
+    );
+  });
 
-				res.json(todos);
-			});
-		});
-	});
+  app.get('*', (req, res) => {
+    res.sendfile('./assets/index.html');
+  });
 
-
-	app.get('*', (req, res) => {
-		res.sendfile('./assets/index.html');
-	});
-
+  app.patch('/completeTask', (req, res) => {
+    Task.findByIdAndUpdate({ _id: req.body.id }, { $set: { done: req.body.done } }, { new: true }, function(err, task) {
+      if (err) res.send(err);
+      res.send(task);
+    });
+  });
 };
--- assets/js/services/task.service.js
+++ assets/js/services/task.service.js
@@ -1,27 +1,27 @@
-(function () {
-	angular.module('todoApp')
-		.service('taskService', ['$http', taskService]);
+(function() {
+  angular.module('todoApp').service('taskService', ['$http', taskService]);
 
-	function taskService($http) {
+  function taskService($http) {
+    var service = {
+      addTask: addTask,
+      getAllTasks: getAllTasks,
+      deleteTask: deleteTask,
+      changeTaskDone: changeTaskDone
+    };
+    return service;
 
-		var service = {
-			addTask: addTask,
-			getAllTasks: getAllTasks,
-			deleteTask:deleteTask
-		};
-		return service;
+    function getAllTasks() {
+      return $http.get('getAllTasks');
+    }
 
-		function getAllTasks() {
-			return $http.get('getAllTasks')
-		}
-
-		function addTask(data) {
-			return $http.post('createTask', data)
-		}
-		function deleteTask(id) {
-			return $http.delete('/tasks/'+id);
-		}
-		
-		
-	}
+    function addTask(data) {
+      return $http.post('createTask', data);
+    }
+    function deleteTask(id) {
+      return $http.delete('/tasks/' + id);
+    }
+    function changeTaskDone(data) {
+      return $http.patch('/completeTask', data);
+    }
+  }
 })();
