44791c6826d28ccd8a49c00f6ed97166cbba2c91
Nadiia Bilozerova <nadiia.bilozerova@synapse.com>
2018-04-24 18:41:38 +0300

    try to do tabs registration

--- assets/js/components/menu/menu.component.js
+++ assets/js/components/menu/menu.component.js
@@ -1,18 +1,12 @@
-(function () {
-	angular.module('todoApp').component('menu', {
-		templateUrl: 'js/components/menu/menu.template.html',
-		controller: [MenuComponent]
-	});
+(function() {
+  angular.module('todoApp').component('menu', {
+    templateUrl: 'js/components/menu/menu.template.html',
+    controller: [MenuComponent]
+  });
 
-	function MenuComponent() {
-		var $ctrl = this;
+  function MenuComponent() {
+    var $ctrl = this;
 
-		$ctrl.menuLinks = [
-			{sref: 'home', text: 'Home'},
-			{sref: 'contacts', text: 'Contacts'},
-			{sref: 'task-list', text: 'Task-list'},
-			{sref: 'auth', text: 'Registration'}
-		]
-	}
-
+    $ctrl.menuLinks = [{ sref: 'home', text: 'Home' }, { sref: 'contacts', text: 'Contacts' }, { sref: 'task-list', text: 'Task-list' }, { sref: 'auth', text: 'Login / Registration' }];
+  }
 })();
