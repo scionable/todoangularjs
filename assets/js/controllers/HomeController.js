6ef3b4f73330d0c27f7de40a2b9556ca051767e0
Nadiia Bilozerova <nadiia.bilozerova@synapse.com>
2018-04-24 18:10:07 +0300

    added ng-show for counting and hide zero value

--- assets/js/controllers/HomeController.js
+++
@@ -1,8 +0,0 @@
-angular.module('todoApp')
-	.controller('HomeController', ['getAllTasks', HomeController]);
-
-function HomeController(getAllTasks) {
-	var $ctrl = this;
-
-	$ctrl.allTasks = getAllTasks;
-}
