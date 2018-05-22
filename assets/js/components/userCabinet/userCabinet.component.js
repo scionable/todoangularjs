80f963e5b24939fe71604a8da93b78d5bbcc0a42
Nadiia Bilozerova <nadiia.bilozerova@synapse.com>
2018-05-22 11:17:17 +0300

    fixed home task

--- assets/js/components/userCabinet/userCabinet.component.js
+++ assets/js/components/userCabinet/userCabinet.component.js
@@ -37,18 +37,23 @@
     // close popup module /
   }
 })();
+
 function mergeArrays(arr1, arr2) {
-  let arr = arr1.concat(arr2).sort(function sort(a, b) {
-    return a - b;
-  });
+  let arr = arr1.concat(arr2);
+  console.log('arr', arr);
   let obj = {};
   for (let i = 0; i < arr.length; i++) {
     let key = arr[i];
     obj[key] = true;
   }
-  return Object.keys(obj);
+  console.log(' obj', obj);
+  return Object.keys(obj)
+    .map(iter => parseInt(iter))
+    .sort(function sort(a, b) {
+      return a < b ? false : true;
+    });
 }
 
-//console.log(mergeArrays([1, 2, 3, 4], [5, 6, 7, 8]), [1, 2, 3, 4, 5, 6, 7, 8], 'Basic tests');
+console.log(mergeArrays([-13, -9, 2, 4, 15, 32, 42, 74, 90, 216], [-8, -3, -2, 0, 5, 6, 7, 8, 102, 134]), [-13, -9, -8, -3, -2, 0, 2, 4, 5, 6, 7, 8, 15, 32, 42, 74, 90, 102, 134, 216], 'Basic tests');
 console.log(mergeArrays([-3, 0, 3, 4], [-2, -1, 1, 2]), [-3, -2, -1, 0, 1, 2, 3, 4], 'Basic tests');
 console.log(mergeArrays([1, 3, 5, 7, 9, 11, 12], [1, 2, 3, 4, 5, 10, 12]), [1, 2, 3, 4, 5, 7, 9, 10, 11, 12], 'Basic tests');
