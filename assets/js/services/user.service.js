2ce819798540780ad0e2af4e07f4c3d62329673c
Arthor <arthor_serdyuk@ukr.net>
2018-05-23 02:07:01 +0300

    refactor userService, added events for user logged in and logout

--- assets/js/services/user.service.js
+++ assets/js/services/user.service.js
@@ -1,66 +1,63 @@
-(function() {
-  angular.module('todoApp').service('userService', ['$http', '$window', '$rootScope', userService]);
+(function () {
+	angular.module('todoApp').service('userService', ['$http', '$q', '$window', '$rootScope', userService]);
 
-  function userService($http, $window, $rootScope) {
-    var service = {
-      user: null,
-      registerUser: registerUser,
-      loginUser: loginUser,
-      saveUser: saveUser,
-      getUserFromlocalStorage: getUserFromlocalStorage,
-      userLogout: userLogout,
-      additionalInfoUser: additionalInfoUser,
-      regexName: /^([а-яё]+|[a-z]+)$/i,
-      regexPass: /^[a-z0-9_-]{3,16}$/,
-      regexEmail: /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/
-    };
-    if (service.user === null) {
-      service.user = getUserFromlocalStorage();
-    }
+	function userService($http, $q, $window, $rootScope) {
 
-    return service;
+		var service = {
+			registerUser: registerUser,
+			loginUser: loginUser,
+			getUserFromlocalStorage: getUserFromlocalStorage,
+			userLogout: userLogout,
+			additionalInfoUser: additionalInfoUser,
+			regexName: /^([а-яё]+|[a-z]+)$/i,
+			regexPass: /^[a-z0-9_-]{3,16}$/,
+			regexEmail: /^([a-z0-9_\.-]+)@([a-z0-9_\.-]+)\.([a-z\.]{2,6})$/
+		};
 
-    function registerUser(data) {
-      return $http.post('/registerUser', data).then(function(response) {
-        if (typeof response.data !== 'string') {
-          saveUser(response.data);
-          service.user = getUserFromlocalStorage();
-        }
-        return response;
-      });
-    }
+		return service;
 
-    function loginUser(data) {
-      var $ctrl = this;
-      return $http.post('/login', data).then(function(response) {
-        if (typeof response.data !== 'string') {
-          saveUser(response.data);
-          service.user = getUserFromlocalStorage();
-          $rootScope.user = service.user;
-        }
-        return response;
-      });
-    }
+		function registerUser(data) {
+			return $http.post('/registerUser', data).then(function (response) {
+				if (typeof response.data !== 'string') {
+					saveUser(response.data);
+					service.user = response.data;
+				}
+				return response;
+			});
+		}
 
-    function saveUser(user) {
-      var serialUser = JSON.stringify(user);
-      $window.localStorage.setItem('user-token', serialUser);
-    }
+		function loginUser(data) {
+			return $http.post('/login', data)
+				.then(function (resp) {
+					if (!(typeof resp.data !== 'string')) return $q.reject(resp.data);
+					saveUser(resp.data);
+					service.user = resp.data;
+				}).then(function () {
+					$rootScope.$broadcast('user-login', {user: service.user});
+				}).then(function () {
+					return $q.resolve('You successfully logged in');
+				})
+		}
 
-    function getUserFromlocalStorage() {
-      return JSON.parse($window.localStorage.getItem('user-token'));
-    }
+		function saveUser(user) {
+			var serialUser = JSON.stringify(user);
+			$window.localStorage.setItem('user-token', serialUser);
+		}
 
-    function userLogout() {
-      service.user = null;
-      return $window.localStorage.removeItem('user-token');
-    }
+		function getUserFromlocalStorage() {
+			return JSON.parse($window.localStorage.getItem('user-token'));
+		}
 
-    function additionalInfoUser(user) {
-      service.user = Object.assign(service.user, user);
-      saveUser(service.user);
+		function userLogout() {
+			service.user = null;
+			$window.localStorage.removeItem('user-token');
+			$rootScope.$broadcast('user-logout', {user: service.user});
+		}
 
-      //return $http.put('/users/' + service.user._id, service.user);
-    }
-  }
+		function additionalInfoUser(user) {
+			service.user = Object.assign(service.user, user);
+			saveUser(service.user);
+		}
+
+	}
 })();
