2b4dece511f24ccc8d8baa4c4ef9461f613d1dc3
Arthor <arthor_serdyuk@ukr.net>
2018-05-24 01:52:22 +0300

    refactored regictration method

--- assets/js/components/registration-form/registrationForm.component.js
+++ assets/js/components/registration-form/registrationForm.component.js
@@ -1,37 +1,30 @@
-(function() {
-  angular.module('todoApp').component('registrationForm', {
-    templateUrl: 'js/components/registration-form/registrationForm.template.html',
-    controller: ('registrationFormController', ['userService', 'helperService', '$timeout', '$rootScope', '$state', registrationFormController])
-  });
+(function () {
+	angular.module('todoApp').component('registrationForm', {
+		templateUrl: 'js/components/registration-form/registrationForm.template.html',
+		controller: ('registrationFormController', ['userService', 'helperService', '$timeout', '$rootScope', '$state', registrationFormController])
+	});
 
-  function registrationFormController(userService, helperService, $timeout, $rootScope, $state) {
-    let $ctrl = this;
-    $ctrl.registerUser = registerUser;
+	function registrationFormController(userService, helperService, $timeout, $rootScope, $state) {
+		let $ctrl = this;
+		$ctrl.registerUser = registerUser;
 
-    $ctrl.name = userService.regexName;
-    $ctrl.pass = userService.regexPass;
-    $ctrl.email = userService.regexEmail;
+		$ctrl.name = userService.regexName;
+		$ctrl.pass = userService.regexPass;
+		$ctrl.email = userService.regexEmail;
 
-    function registerUser(ev, newUser) {
-      ev.preventDefault();
-      userService
-        .registerUser(newUser)
-        .then(function(response) {
-          if (typeof response.data === 'string') {
-            $ctrl.errorMessage = response.data;
-            helperService.hideErrorMessage($ctrl.errorMessage);
-          } else {
-            $state.go('home');
-          }
+		function registerUser(ev, newUser) {
+			ev.preventDefault();
+			userService
+				.registerUser(newUser)
+				.then(function (resp) {
+					//todo resp contains successfully logged in message, create toast for it
+					helperService.clearForm($ctrl.user);
+				})
+				.catch(function (err) {
+					$ctrl.errorMessage = err;
+					helperService.hideErrorMessage($ctrl.errorMessage);
+				})
+		}
 
-          return response.data;
-        })
-        .then(function() {
-          helperService.clearForm($ctrl.user);
-        })
-        .catch(function(err) {
-          console.log('Error now is:', err);
-        });
-    }
-  }
+	}
 })();
