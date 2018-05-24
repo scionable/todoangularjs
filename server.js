0f6935a41305e9e7759c5966df3826914d97e2fe
Arthor <arthor_serdyuk@ukr.net>
2018-05-25 00:55:11 +0300

    Increased limit for body parcer

--- server.js
+++ server.js
@@ -8,10 +8,11 @@ const app = express();
 
 app.use(express.static(__dirname + '/assets'));                // set the static files location /assets/img will be /img for users
 // app.use(morgan('dev'));
-app.use(bodyParser.urlencoded({'extended': 'true'}));          // parse application/x-www-form-urlencoded
-app.use(bodyParser.json());                                    // parse application/json
-app.use(bodyParser.json({type: 'application/vnd.api+json'}));  // parse application/vnd.api+json as json
+app.use(bodyParser.urlencoded({'extended': 'true', limit: '50mb'}));          // parse application/x-www-form-urlencoded
+app.use(bodyParser.json({limit: '50mb'}));                                    // parse application/json
+app.use(bodyParser.json({type: 'application/vnd.api+json',limit: '50mb'}));  // parse application/vnd.api+json as json
 app.use(methodOverride());
+
 
 mongoose.connect('mongodb://localhost/test')
 	.then(() => console.log('connected'))
