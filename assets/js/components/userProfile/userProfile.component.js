5a658891431878d1f24702d5aa3aff463c0a20f2
Arthor <arthor_serdyuk@ukr.net>
2018-06-06 00:48:07 +0300

    minor refactor

--- assets/js/components/userProfile/userProfile.component.js
+++ assets/js/components/userProfile/userProfile.component.js
@@ -1,45 +1,37 @@
-(function() {
-  angular.module("todoApp").component("userProfile", {
-    templateUrl: "js/components/userProfile/userProfile.template.html",
-    controller: ("userProfileController",
-    ["$rootScope", "userService", userProfileController])
-  });
+(function () {
+	angular.module('todoApp').component('userProfile', {
+		templateUrl: 'js/components/userProfile/userProfile.template.html',
+		controller: ('userProfileController', ['$rootScope', 'userService', userProfileController])
+	});
 
-  function userProfileController($rootScope, userService) {
-    let $ctrl = this;
-    $ctrl.user = userService.user;
+	function userProfileController($rootScope, userService) {
+		let $ctrl = this;
+		$ctrl.user = userService.user;
+		$ctrl.displayAvatar = $ctrl.user.avatar || '';
 
-    if ($ctrl.user === null) {
-      $ctrl.displayAvatar = "";
-      $ctrl.exist = true;
-    } else {
-      $ctrl.exist = false;
-      $ctrl.displayAvatar = $ctrl.user.avatar;
+		$ctrl.onAvatarChange = onAvatarChange;
+		$ctrl.submit = submit;
 
-      $ctrl.userAdditionalInfo = {
-        displayAvatar: $ctrl.user.avatar,
-        aboutme: $ctrl.user.aboutme,
-        name: $ctrl.user.name,
-        screenName: $ctrl.user.screenName,
-        id: $ctrl.user._id
-      };
-    }
+		$ctrl.userAdditionalInfo = {
+			displayAvatar: $ctrl.user.avatar || '',
+			aboutme: $ctrl.user.aboutme || '',
+			name: $ctrl.user.name,
+			screenName: $ctrl.user.screenName || '',
+			id: $ctrl.user._id
+		};
 
-    $ctrl.onAvatarChange = onAvatarChange;
-    $ctrl.submit = submit;
+		function onAvatarChange(event) {
+			$ctrl.displayAvatar = $ctrl.userAdditionalInfo.displayAvatar = arguments[5].base64;
+			$rootScope.$broadcast('userAvatarUpdated', $ctrl.userAdditionalInfo.displayAvatar);
+		}
 
-    function onAvatarChange(event) {
-      $ctrl.displayAvatar = $ctrl.userAdditionalInfo.displayAvatar =
-        arguments[5].base64;
-    }
+		function submit(userAdditionalInfo) {
+			delete userAdditionalInfo.avatar;
+			userService.updateUserInfo(userAdditionalInfo)
+				.then(function (resp) {
+					console.log(resp);
+				})
+		}
 
-    function submit(userAdditionalInfo) {
-      delete userAdditionalInfo.avatar;
-      $rootScope.$broadcast(
-        "userAvatarUpdated",
-        $ctrl.userAdditionalInfo.displayAvatar
-      );
-      userService.updateUserInfo(userAdditionalInfo).then(function(resp) {});
-    }
-  }
+	}
 })();
