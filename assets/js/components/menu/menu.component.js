5c0180066d0403839b582c033377e36395af6dfb
Arthor <arthor_serdyuk@ukr.net>
2018-06-06 00:46:07 +0300

    Added live update menu depends on logged / logout user logic

--- assets/js/components/menu/menu.component.js
+++ assets/js/components/menu/menu.component.js
@@ -1,28 +1,38 @@
-(function() {
-  angular.module("todoApp").component("menu", {
-    templateUrl: "js/components/menu/menu.template.html",
-    controller: ["$scope", "userService", MenuComponent]
-  });
+(function () {
+	angular.module("todoApp").component("menu", {
+		templateUrl: "js/components/menu/menu.template.html",
+		controller: ["$scope", "userService", MenuComponent]
+	});
 
-  function MenuComponent($scope, userService) {
-    var $ctrl = this;
-    $ctrl.user = userService.user;
-    $ctrl.menuLinks = [
-      { sref: "home", text: "Home" },
-      { sref: "userProfile", text: "Profile" },
-      { sref: "task-list", text: "Task-list" },
-      { sref: "auth", text: "Login / Registration" }
-    ];
+	function MenuComponent($scope, userService) {
+		var $ctrl = this;
+		$ctrl.user = userService.user;
+		$ctrl.menuLinks = [
+			{sref: "home", text: "Home", isShow: true},
+			{sref: "userProfile", text: "Profile", isShow: !!$ctrl.user, shouldUpdate: true},
+			{sref: "task-list", text: "Task-list", isShow: !!$ctrl.user, shouldUpdate: true},
+			{sref: "auth", text: "Login / Registration", isShow: true}
+		];
 
-    $scope.$on("user-logout", function(event, args) {
-      $ctrl.user = args.user;
-    });
+		function updateMenu(){
+			$ctrl.menuLinks.forEach(function (menuLink) {
+				if(!menuLink.shouldUpdate) return;
+				return menuLink.isShow = !!$ctrl.user;
+			})
+		}
 
-    $scope.$on("user-login", function(event, args) {
-      $ctrl.user = args.user;
-    });
-    $scope.$on("userAvatarUpdated", function(event, args) {
-      $ctrl.user.avatar = args;
-    });
-  }
+		$scope.$on("user-logout", function (event, args) {
+			$ctrl.user = args.user;
+			updateMenu();
+		});
+
+		$scope.$on("user-login", function (event, args) {
+			$ctrl.user = args.user;
+			updateMenu();
+		});
+
+		$scope.$on("userAvatarUpdated", function (event, args) {
+			$ctrl.user.avatar = args;
+		});
+	}
 })();
