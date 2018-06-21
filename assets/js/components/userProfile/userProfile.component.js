e29efeb49f7fe549d445fa816f749283ba206def
Arthor <arthor_serdyuk@ukr.net>
2018-06-22 01:11:04 +0300

    minor fixes

--- assets/js/components/userProfile/userProfile.component.js
+++ assets/js/components/userProfile/userProfile.component.js
@@ -1,34 +1,29 @@
-(function() {
-  angular.module("todoApp").component("userProfile", {
-    templateUrl: "js/components/userProfile/userProfile.template.html",
-    controller: ("userProfileController",
-    [
-      "$rootScope",
-      "userService",
-      "toastr",
-      "helperService",
-      userProfileController
-    ])
-  });
+(function () {
+	angular.module("todoApp").component("userProfile", {
+		templateUrl: "js/components/userProfile/userProfile.template.html",
+		controller: ("userProfileController", ["$rootScope", "userService", "toastr", "helperService", userProfileController])
+	});
 
-  function userProfileController(
-    $rootScope,
-    userService,
-    toastr,
-    helperService
-  ) {
-    let $ctrl = this;
-    $ctrl.user = userService.user;
+	function userProfileController(
+		$rootScope,
+		userService,
+		toastr,
+		helperService
+	) {
+		let $ctrl = this;
+		$ctrl.user = userService.user;
 
-    $ctrl.displayAvatar = $ctrl.user.avatar;
+		$ctrl.displayAvatar = $ctrl.user.avatar;
+		$ctrl.onAvatarChange = onAvatarChange;
+		$ctrl.submit = submit;
 
-    $ctrl.userAdditionalInfo = {
-      displayAvatar: $ctrl.user.avatar,
-      aboutme: $ctrl.user.aboutme,
-      name: $ctrl.user.name,
-      screenName: $ctrl.user.screenName,
-      id: $ctrl.user._id
-    };
+		$ctrl.userAdditionalInfo = {
+			displayAvatar: $ctrl.user.avatar,
+			aboutme: $ctrl.user.aboutme,
+			name: $ctrl.user.name,
+			screenName: $ctrl.user.screenName,
+			id: $ctrl.user._id
+		};
 
 		function onAvatarChange(event) {
 			$ctrl.displayAvatar = $ctrl.userAdditionalInfo.displayAvatar = arguments[5].base64;
@@ -37,22 +32,14 @@
 
 		function submit(userAdditionalInfo) {
 			delete userAdditionalInfo.avatar;
-			userService.updateUserInfo(userAdditionalInfo)
-				.then(function (resp) {
-					console.log(resp);
-				})
+			$rootScope.$broadcast(
+				"userAvatarUpdated",
+				$ctrl.userAdditionalInfo.displayAvatar
+			);
+			userService.updateUserInfo(userAdditionalInfo).then(function (resp) {
+				toastr.success(resp, "Success!");
+				helperService.clearForm(userAdditionalInfo);
+			});
 		}
-
-    function submit(userAdditionalInfo) {
-      delete userAdditionalInfo.avatar;
-      $rootScope.$broadcast(
-        "userAvatarUpdated",
-        $ctrl.userAdditionalInfo.displayAvatar
-      );
-      userService.updateUserInfo(userAdditionalInfo).then(function(resp) {
-        toastr.success(resp, "Success!");
-        helperService.clearForm(userAdditionalInfo);
-      });
-    }
-  }
+	}
 })();
